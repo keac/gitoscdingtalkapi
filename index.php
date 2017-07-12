d38723e1cf392c7d45edcd988f90a465d7d4f0cb
keac <admin@wgpsec.org>
2017-07-12 10:23:38 +0800

    Fix bug

--- index.php
+++ index.php
@@ -1,187 +1,262 @@
 <?php
-/**
+/**************************************
+ *
  * Created by PhpStorm.
  * User: keac
  * Date: 2017/3/14 0014
  * Time: 14:09
- */
+ * Email:admin@northme.com
+ * Hangzhou Northme Technology Co. Ltd
+ *
+ *  @@@@@@@@@@@@~~:-:~:::::~~:;;;@@@@@@@@@@@@@@@@,@@@,@@@@@@@@@@,@@,,,,,,,,@@@ ,,,,,,@-,@@@,@@@@@@,@@@@@@,,@@@@@@@@@@@@@@@@@
+ *  @@@@@@@@@@@~:~~~~::;:::::~:;;;@@@@@@@@@@@@@@@,@@@,@@@@@@@@@,,@@@@@@@@,-@@@-,,,,@@@@,,@@,@@@@@@,@@@@@@,,@@@@@@@@@@@@@@@@@
+ *  @@@@@@@@@@:::;::~~:~::::;~:;;;!@@@@@@@@@@@@@@,@@@,@@@@@@@@,,@@@@@@@@,,@@@@@@@,,@@@@@,,@,@@@@@@,@@,,,,,,,,,,@@@@@@@@@@@@@
+ *  @@@@@@@@@::::::::;:;:;;;;;!;;!;;@@@@@@@@@@@@@,@@@,@@@@,@@,,@@@@@@@@,,@@@@@@@@,,@@@@@@,@,@@@@,,,,,@@@@,,@@@@@@@@@@@@@@@@@
+ *  @@@@@@@@::@@@@@@:::::;;;;;!:;;;;@@@@@@@@,,,,,,@@@,@@,,,@@,@@@,@@@@,,,,@@@@,,,,,,,@,@@@@,@@@@@@,@@@@@@,,@@@@@@@@@@@@@@@@@
+ *  @@@@@@@@:~;:;:@@@;;:;:;!;;;;;!;;*@@@@@@@@@@@@,@@@,,,,@@@@@@@,,@@@,,@@,,@@@@@@,,@@@,,@@@,@@@@@@,@@@@@@,,@@@@@@@@@@@@@@@@@
+ *  @@@@@@@~::~:;:@@@@;:;;;;;!;;;!::;@@@@@@@@@@@@,@@@,@@@@@@@@@,,@-,,,@@@@,,,@@@@,,~@@@,,~@,@@@@@@,@,@,,,,,,,,@@@@@@@@@@@@@@
+ *  @@@@@@@;::::@@@@@@@:;!:;:;;!;;;:;@@@@@@@@@@@@,@@@,@@@@@@@@,,,@,-@@@@@@@@@@@@,,,,-@@@,,@,@@@@@@,,,@@,@@@@@,@@@@@@@@@@@@@@
+ *  @@@@@@@;;:@@@@@@@@@@;;;:;!!!;;!!;*@@@@@@@@@@@,@@@,@@@@@@@,,,,@@,,,,,,,,,@@@,,,,@,,@@@@@,@@@-,,,@@@@,-@@@,,@@@@@@@@@@@@@@
+ *  @@@@@@;;!@@@@@@@@@@@@!;;!;!!!;!!!;@@@@@@@@@@@,@@@,@@@@@@@-@,,@@----,----@@-,@,,@@@@-,,,,,,@,@@,@@@@@,@@-,@@@@@@@@@@@@@@@
+ *  @@@@@@;;@@@@@@@@@@@@@@*;;!;;!!;!*!@@@@@@@@@@@,@@@,@@@@,@@@@,,@@@@@@,@@@@@@,@@,,@@,,,,,@,@@@@@@,@@@@@,,@, @@@@@@@@@@@@@@@
+ *  @@@@@@;~@@@@@@@@@@@@@@@@;!;;;!;!;!@@@@@@@@@,,,@@@,@@@@,@@@@,,@@@@@@,@@@@@@-@@,,@@@@@@@@,@@@@@@,@@@@@@,,-@@@@@@@@@@@@@@@@
+ *  @@@@@@;@@@@@@@@@@@@@@@@@@!!!!!!!@*@@@@@@,,,,@,@@@,@@@@,@@@@,,@@@@@@,@@@@@@@@@,,@@@@@@@@,@@@@@@,@@@@@,,,, @@@@@@@@@@@@@@@
+ *  @@@@@@!@@@@@@@@@@@@@@@@@@@@@@@@@@;@@@@@@,@@@@,@@@,@@@,,@@@@,,@,,,,,,,,,,-@@@@,,@@@@@@@@,@@@@@@,@@@@,,@@,,,@@@@@@@@@@@@@@
+ *
+ ***************************************/
 
-$git_Data=json_decode($GLOBALS['HTTP_RAW_POST_DATA']);
 
-if($git_Data->password=="Northme"){ //Set password
-$hook_name=$git_Data->hook_name;
 
-/*Push*/
-function fnPush($data){
-$commit_arr=$data -> commits;
-$commit_messages="";
-foreach ($commit_arr as $commit)
+
+/***
+ *
+ * Basic setting
+ *
+ ***/
+
+$pwd = "Northme"; //Set password
+$access_token = "xxx"; // Set Api token
+
+/* End of Setting */
+
+
+$git_Data = json_decode($GLOBALS['HTTP_RAW_POST_DATA']); // Get Git@osc data
+
+if ($git_Data->password != $pwd) {       // password
+    header('HTTP/1.0 404 Not Found');
+    exit();
+}
+
+// Start to Get the Git@osc data analysis
+
+$title = $hook_name = $git_Data->hook_name; // Get the action
+
+
+/*
+ * Function the action
+ * Push  note merge_request issue tag
+ *
+ **/
+
+
+// Push
+function fnPush($data)
 {
-    $commit_messages.="
-commits id：".$commit -> id ."\n
-commits message：".$commit -> message ."\n
-commits 时间：".$commit ->timestamp ."\n
-";
-}
-$messages=
- "Git@OSC ".$data -> repository -> name."Push 提示\n
-分支：".$data -> ref ."\n
-提交者：".$data -> user_name ."\n
-提交时间：".$data -> user -> time ."\n
-Commits个数：".$data -> total_commits_count ."\n
----详情---
-$commit_messages
----仓库信息--
-仓库名称：".$data -> repository -> name ."\n
-仓库地址：".$data -> repository -> url ." \n
-仓库描述：".$data -> repository -> description." \n
-仓库主页：".$data -> repository -> homepage ."\n
-";
-return $messages;
-}
-/* Note */
-function fn_Note($data){
+    // Get the Commit
+    $commit_arr = $data->commits;
+    $commit_messages = "";
 
-    $messages="
-Git@osc ".$data -> noteable_type ." 提示\n
-内容：".$data -> note ."\n
-Id：".$data -> noteable_id ."\n
-作者：".$data -> author -> user_name."\n
-地址：".$data -> url."\n
----项目--
-项目名称：".$data -> project ->name ."\n
-项目路径：".$data -> project ->path ."\n
-项目描述：".$data -> project ->description ."\n
-项目地址：".$data -> project ->url ."\n
-项目Git：".$data -> project ->git_http_url ."\n
----仓库--
-仓库名称：".$data -> repository ->name ."\n
-仓库链接：".$data -> repository ->url ."\n
-仓库描述：".$data -> repository ->description ."\n
-";
+    //decompose Commit
+    foreach ($commit_arr as $commit) {
+
+        // Processing character
+        $commit->id = substr($commit->id, 0, 7);
+        $commit->timestamp = date($commit->timestamp);
+
+        // Add character
+        $commit_messages .= <<<EOF
+                NHL \n id： {$commit->id}
+                NHL Time： {$commit->timestamp}
+                NHL >  {$commit->message}
+EOF;
+    }
+
+    $messages = <<< EOF
+            \n ## {$data->repository->name}   Push 提示
+            \n 分支：{$data->ref} 
+            NHL 提交者：{$data->user_name}  
+            NHL 提交时间：{$data->user->time} 
+            NHL 共{$data->total_commits_count}个 Commit
+            NHL #### Commit 详情
+             {$commit_messages}
+            NHL #### 仓库
+            NHL 仓库名称：{$data->repository->name}
+            NHL > {$data->repository->description}
+            NHL \n [仓库地址]( {$data->repository->url} )
+            NHL    [仓库主页]( {$data->repository->homepage} )
+EOF;
     return $messages;
 }
 
-/* PullRequest */
-function fnMerge_request($data){
-    $messages="
-Git@osc PullRequest 提示\n
-iid：".$data -> iid ."\n
-title：".$data -> title ."\n
-body：".$data -> body ."\n
-state：".$data -> state ."\n
-merge_status：".$data -> merge_status ."\n
-Note作者：".$data -> author -> user_name."\n
-Note地址：".$data -> url."\n
-key1：".$data -> key1."\n
-key2：".$data -> key2."\n
-
-源分支：".$data -> source_branch ."\n
----源项目--
-源项目名称：".$data ->source_repo -> project ->name ."\n
-源项目路径：".$data ->source_repo -> project ->path ."\n
-源项目描述：".$data ->source_repo -> project ->description ."\n
-源项目地址：".$data ->source_repo -> project ->url ."\n
-源项目Git：".$data ->source_repo -> project ->git_http_url ."\n
-源项目命名空间：".$data ->source_repo -> project ->namespace ."\n
-源项目有路径的空间：".$data ->source_repo -> project ->path_with_namespace ."\n
-源项目分支：".$data ->source_repo -> project ->default_branch ."\n
----源仓库--
-源仓库名称：".$data ->source_repo ->  repository ->name ."\n
-源仓库链接：".$data ->source_repo ->  repository ->url ."\n
-源仓库描述：".$data ->source_repo ->  repository ->description ."\n
-
----目标仓库--
-目标分支：".$data -> target_branch ."\n
----目标项目--
-目标项目名称：".$data ->target_repo -> project ->name ."\n
-目标项目路径：".$data ->target_repo -> project ->path ."\n
-目标项目描述：".$data ->target_repo -> project ->description ."\n
-目标项目地址：".$data ->target_repo -> project ->url ."\n
-目标项目Git：".$data ->target_repo -> project ->git_http_url ."\n
-目标项目命名空间：".$data ->target_repo -> project ->namespace ."\n
-目标项目有路径的空间：".$data ->target_repo -> project ->path_with_namespace ."\n
-目标项目分支：".$data ->target_repo -> project ->default_branch ."\n
----目标仓库--
-目标仓库名称：".$data -> target_repo-> repository ->name ."\n
-目标仓库链接：".$data -> target_repo-> repository ->url ."\n
-目标仓库描述：".$data -> target_repo-> repository ->description ."\n
-
-";
+// Note
+function fn_Note($data)
+{
+    $Note_msg= str_replace(array("\r\n", "\r"), 'NHL', $data->note);
+    $messages = <<<EOF
+       \n  ##  {$data->project->name} {$data->noteable_type} 提示
+        NHL Id： {$data->noteable_id}
+        NHL 作者：{$data->author->user_name}
+        NHL --内容--
+        NHL  {$Note_msg}
+        NHL --内容--
+        NHL #### 仓库
+        NHL 仓库名称：{$data->repository->name} 
+        NHL [仓库链接]({$data->repository->url})
+        NHL 仓库描述：
+        NHL > {$data->repository->description}
+EOF;
     return $messages;
+}
+
+// PullRequest
+function fnMerge_request($data)
+{
+    $Merge_request_body= str_replace(array("\r\n", "\r"), 'NHL', $data->body);
+    $messages = <<<EOF
+        \n  ##  {$data->source_repo->repository->name} PullRequest 提示
+        \n iid： #{$data->iid} 
+        NHL 标题： {$data->title} 
+        NHL --说明---
+        NHL  {$Merge_request_body} 
+        NHL \n --说明---
+        NHL \n 状态： {$data->state} 
+        NHL 合并状态： {$data->merge_status} 
+        NHL 提起者：{$data->author->user_name} 
+        NHL > {$data->source_repo->project->path_with_namespace} : {$data->source_branch}
+        NHL ↓↓↓↓↓↓↓↓↓↓↓
+        NHL  {$data->target_repo->project->path_with_namespace} : {$data->target_branch}
+
+        NHL ### 源项目
+        NHL 项目名称：{$data->source_repo->project->name}
+        NHL > {$data->source_repo->project->description}
+        NHL [项目地址]({$data->source_repo->project->url})
+        NHL ### 源仓库
+        NHL 仓库名称：{$data->source_repo->repository->name}
+        NHL > {$data->source_repo->repository->description}
+        NHL \n [仓库链接]({$data->source_repo->repository->url})
+        NHL ### 目标项目
+        NHL 目标项目名称：{$data->target_repo->project->name}
+        NHL > {$data->target_repo->project->description}
+        NHL \n [目标项目地址]({$data->target_repo->project->url})
+        NHL ### 目标仓库
+        NHL 目标仓库名称：{$data->target_repo->repository->name}
+        NHL > {$data->target_repo->repository->description}
+        NHL \n [目标仓库链接]({$data->target_repo->repository->url})
+
+EOF;
+    return $messages;
+
 }
 
 /* Issue */
-function fn_Issue($data){
+function fn_Issue($data)
+{
+    $assignee=empty($data->assignee) ? "未指定":$data->assignee->user_name;
+    $milestone=empty($data->milestone) ? "没有里程碑":$data->milestone;
+    $Issue_request_body= str_replace(array("\r\n", "\r"), 'NHL', $data->description);
+    $messages = <<<EOF
+        \n ## {$data->project->name} Issue 提示
+        NHL iid： {$data->iid}
+        NHL 名称：{$data->title} 
+        NHL --描述--
+        NHL {$Issue_request_body} 
+        NHL --描述--
+        NHL 状态：{$data->state}
+        NHL 指定： {$assignee} 
+        NHL 里程碑：{$milestone}
+        NHL 提起者：{$data->user->username} 
 
- $messages="
-Git@osc Issue 提示\n
-iid：".$data -> iid ."\n
-名称：".$data -> title ."\n
-描述：".$data -> description ."\n
-状态：".$data -> state ."\n
-assignee：".$data -> assignee."\n
-milestone：".$data -> milestone."\n
-提起者：".$data -> user -> username ."\n
+        NHL ### 项目
+        NHL 项目名称：{$data->project->name}
+        NHL 项目分支：{$data->project->default_branch}
+        NHL [项目地址]({$data->project->url})
 
----项目--
-项目名称：".$data -> project ->name ."\n
-项目路径：".$data -> project ->path ."\n
-项目描述：".$data -> project ->description ."\n
-项目地址：".$data -> project ->url ."\n
-项目Git：".$data -> project ->git_http_url ."\n
-项目分支：".$data -> project ->default_branch ."\n
+        NHL ### 仓库
+        NHL 仓库名称：{$data->repository->name}
+        NHL [仓库链接]({$data->repository->url})
+        NHL > {$data->repository->description}
 
----仓库--
-仓库名称：".$data -> repository ->name ."\n
-仓库链接：".$data -> repository ->url ."\n
-仓库描述：".$data -> repository ->description ."\n
-";
-        return $messages;
-    }
-    /*Tag*/
-function fn_Tag($data){
+EOF;
 
- $messages="
-Git@osc 新增 Tag  提示\n
-ref：".$data -> ref ."\n
-before：".$data -> before ."\n
-after：".$data -> after ."\n
----项目--
-项目名称：".$data -> project ->name ."\n
-项目路径：".$data -> project ->path ."\n
-项目描述：".$data -> project ->description ."\n
-项目地址：".$data -> project ->url ."\n
-项目Git：".$data -> project ->git_http_url ."\n
-项目分支：".$data -> project ->default_branch ."\n
----仓库--
-仓库名称：".$data -> repository ->name ."\n
-仓库链接：".$data -> repository ->url ."\n
-仓库描述：".$data -> repository ->description ."\n
-";
- return $messages;
-    }
-switch ($hook_name){
+
+    return $messages;
+}
+
+/*Tag*/
+function fn_Tag($data)
+{
+
+    $messages = <<<EOF
+        \n ## {$data->project->name} 新增 Tag  提示
+        NHL ref：{$data->ref} 
+        NHL Before
+        NHL > {$data->before}
+        NHL \n After
+        NHL > {$data->after}
+        NHL ### 项目
+        NHL 项目名称：{$data->project->name}
+        NHL [项目地址]({$data->project->url})
+        NHL 项目分支：{$data->project->default_branch}
+        NHL ### 仓库
+        NHL 名称：{$data->repository->name}
+        NHL [链接]({$data->repository->url})
+        NHL > {$data->repository->description}
+EOF;
+    return $messages;
+}
+
+
+
+
+switch ($hook_name) {
     case "push_hooks":
-        $messages=fnPush($git_Data);
+        $messages = fnPush($git_Data);
+        $title = $git_Data->repository->name ." Push";
         break;
     case "note_hooks":
-        $messages=fn_Note($git_Data);
+        $messages = fn_Note($git_Data);
+        $title = $git_Data->project->name ." ". $git_Data->noteable_type;
         break;
     case "merge_request_hooks":
-        $messages=fnMerge_request($git_Data);
+        $messages = fnMerge_request($git_Data);
+        $title = $git_Data->source_repo->repository->name . " PullRequest";
         break;
     case "issue_hooks":
-        $messages=fn_Issue($git_Data);
+        $messages = fn_Issue($git_Data);
+        $title = $git_Data->project->name . " issue";
         break;
     case "tag_push_hooks":
-        $messages=fn_Tag($git_Data);
+        $messages = fn_Tag($git_Data);
+        $title = $git_Data->project->name . " New Tag";
         break;
 
     default:
-    $messages="Error ! The ip is ".$_SERVER["REMOTE_ADDR"];
-    break;
+        $messages = "Error ! The ip is" . $_SERVER["REMOTE_ADDR"];
+        break;
 }
+
+
+/******
+ *
+ *
+ *  Happy !!!!!!!!!!!!
+ *  By keac
+ *
+ * */
+
+$messages = str_replace(array("\r\n", "\r"), '', $messages);
+$messages = str_replace("NHL", "&nbsp;&nbsp;\n", $messages);
+
 
 
 /*************
@@ -189,17 +264,42 @@ switch ($hook_name){
  *  To DingTalk
  *
  ************/
-$access_token="";
-$isAtAll=false;
-$data = array ('msgtype' => 'text','text' => array ('content' => $messages,),'at' => array ('atMobiles' => array (),'isAtAll' => $isAtAll,),);
-$data_string = json_encode($data);
-$urls="https://oapi.dingtalk.com/robot/send?access_token=".$access_token;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_HTTPHEADER,array('Content-Type: application/json; charset=utf-8'));
-curl_setopt($ch, CURLOPT_URL, "$urls");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-$retBase = curl_exec($ch);
-curl_close($ch);
+
+
+//    $isAtAll = false;
+//$data = array('msgtype' => 'text', 'text' => array('content' => $messages,), 'at' => array('atMobiles' => array(), 'isAtAll' => $isAtAll,),);
+
+
+$data = array('msgtype' => 'markdown', 'markdown' => array('title' => $title, 'text' => $messages,),);
+
+
+curlDing($data, $access_token);
+
+
+
+/*
+ *
+ * Post to DingTalk
+ *
+ * */
+
+
+function curlDing($data, $access_token){
+
+
+    $data_string = json_encode($data); // To json
+    $urls = "https://oapi.dingtalk.com/robot/send?access_token=" . $access_token; // To DingTalk API
+    /* POST */
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8'));
+    curl_setopt($ch, CURLOPT_URL, "$urls");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    $retBase = curl_exec($ch);
+    curl_close($ch);
+
+    /* End of Post */
+
+    return $retBase;
 }
